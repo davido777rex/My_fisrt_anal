83097ef331a959613c1e45c1dcb319aac8de4d7b
davido777rex <45339438+davido777rex@users.noreply.github.com>
2019-09-28 13:54:57 -0500

    yutaro update
    cumple zero

---
+++ Zeropio/assets/scripts/main.js
@@ -0,0 +1,65 @@
+function open_card() {
+    document.getElementById('outside').className = 'open-card';
+  }
+  
+  function close_card() {
+    document.getElementById('outside').className = '';
+  }
+
+
+  function open_card2() {
+    document.getElementById('inside').className = 'open-card';
+  }
+
+  function close_card2() {
+    document.getElementById('inside').className = '';
+  }
+
+  function open_card3() {
+    document.getElementById('inside2').className = 'open-card';
+  }
+
+  function close_card3() {
+    document.getElementById('inside2').className = '';
+  }
+
+
+  function open_card4() {
+    document.getElementById('inside3').className = 'open-card';
+  }
+
+  function close_card4() {
+    document.getElementById('inside3').className = '';
+  }
+
+  function open_card5() {
+    document.getElementById('inside4').className = 'open-card';
+  }
+
+  function close_card5() {
+    document.getElementById('inside4').className = '';
+  }
+
+  function open_card6() {
+    document.getElementById('inside5').className = 'open-card';
+  }
+
+  function close_card6() {
+    document.getElementById('inside5').className = '';
+  }
+
+  function open_card7() {
+    document.getElementById('inside6').className = 'open-card';
+  }
+
+  function close_card7() {
+    document.getElementById('inside6').className = '';
+  }
+
+  function open_card8() {
+    document.getElementById('inside7').className = 'open-card';
+  }
+
+  function close_card8() {
+    document.getElementById('inside7').className = '';
+  }
